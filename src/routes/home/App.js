bae21c8a9735654d9278aa13d07ca13dd4da8aa2
Marcin Bazanowski <mbazanowski@apptension.com>
2020-03-21 16:47:17 +0100

    restructure initial project - add routes/components dirs, refactor to use scss, remove service worker

---
+++ src/routes/home/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__wrapper">
+        <h1>Sample comment system</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
